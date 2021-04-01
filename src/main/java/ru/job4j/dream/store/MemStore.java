83b59830f43bb2fbabfbc9d898770eb7b828542b
Andrey Masterenko H <masterenko.as@gmail.com>
2021-04-01 07:24:31 +0300

    0. MVC в Servlet. [#466392] - updated: fix save method in MemStore (POST_ID->CANDIDATE_ID)

--- src/main/java/ru/job4j/dream/store/MemStore.java
+++ src/main/java/ru/job4j/dream/store/MemStore.java
@@ -45,7 +45,7 @@ public class MemStore implements Store {
 
     public void save(Candidate candidate) {
         if (candidate.getId() == 0) {
-            candidate.setId(POST_ID.incrementAndGet());
+            candidate.setId(CANDIDATE_ID.incrementAndGet());
         }
         candidates.put(candidate.getId(), candidate);
     }
